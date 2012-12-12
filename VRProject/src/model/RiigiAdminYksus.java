bd6260eb7adb889669510f25cc54591e841f4c28
Hadding <braid.torn@gmail.com>
2012-12-12 12:06:52 +0200

    andmemudel

---
+++ VRProject/src/model/RiigiAdminYksus.java
@@ -0,0 +1,90 @@
+package model;
+
+import java.util.Date;
+
+public class RiigiAdminYksus {
+	private int id;
+	private String avaja;
+	private Date avatud;
+	private String muutja;
+	private Date muudetud;
+	private String sulgeja;
+	private String suletud;
+	private String kood;
+	private String nimetus;
+	private String kommentaar;
+	private Date alates;
+	private Date kuni;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAvaja() {
+		return avaja;
+	}
+	public void setAvaja(String avaja) {
+		this.avaja = avaja;
+	}
+	public Date getAvatud() {
+		return avatud;
+	}
+	public void setAvatud(Date avatud) {
+		this.avatud = avatud;
+	}
+	public String getMuutja() {
+		return muutja;
+	}
+	public void setMuutja(String muutja) {
+		this.muutja = muutja;
+	}
+	public Date getMuudetud() {
+		return muudetud;
+	}
+	public void setMuudetud(Date muudetud) {
+		this.muudetud = muudetud;
+	}
+	public String getSulgeja() {
+		return sulgeja;
+	}
+	public void setSulgeja(String sulgeja) {
+		this.sulgeja = sulgeja;
+	}
+	public String getSuletud() {
+		return suletud;
+	}
+	public void setSuletud(String suletud) {
+		this.suletud = suletud;
+	}
+	public String getKood() {
+		return kood;
+	}
+	public void setKood(String kood) {
+		this.kood = kood;
+	}
+	public String getNimetus() {
+		return nimetus;
+	}
+	public void setNimetus(String nimetus) {
+		this.nimetus = nimetus;
+	}
+	public String getKommentaar() {
+		return kommentaar;
+	}
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}
+	public Date getAlates() {
+		return alates;
+	}
+	public void setAlates(Date alates) {
+		this.alates = alates;
+	}
+	public Date getKuni() {
+		return kuni;
+	}
+	public void setKuni(Date kuni) {
+		this.kuni = kuni;
+	}
+}
---
+++ VRProject/src/model/VoimalikAlluvus.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.Date;
+
+public class VoimalikAlluvus {
+	private int id;
+	private String avaja;
+	private Date avatud;
+	private String muutja;
+	private Date muudetud;
+	private String sulgeja;
+	private String suletud;
+	private String kommentaar;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAvaja() {
+		return avaja;
+	}
+	public void setAvaja(String avaja) {
+		this.avaja = avaja;
+	}
+	public Date getAvatud() {
+		return avatud;
+	}
+	public void setAvatud(Date avatud) {
+		this.avatud = avatud;
+	}
+	public String getMuutja() {
+		return muutja;
+	}
+	public void setMuutja(String muutja) {
+		this.muutja = muutja;
+	}
+	public Date getMuudetud() {
+		return muudetud;
+	}
+	public void setMuudetud(Date muudetud) {
+		this.muudetud = muudetud;
+	}
+	public String getSulgeja() {
+		return sulgeja;
+	}
+	public void setSulgeja(String sulgeja) {
+		this.sulgeja = sulgeja;
+	}
+	public String getSuletud() {
+		return suletud;
+	}
+	public void setSuletud(String suletud) {
+		this.suletud = suletud;
+	}
+	public String getKommentaar() {
+		return kommentaar;
+	}
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}
+}
+
